361a3edb9fd50d949426a96a8627e852f580d622
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2022-01-19 04:36:22 +0530

    Day 2
    Added Fundamentals.java and Operators.java

---
+++ Fundamentals of java/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld{
+    public static void main(String[] args) {
+        if (args[0].equals("increasing")){
+             
+            System.out.println("madhurjya is good");
+        }
+        else{
+            
+            System.out.println("MVDZO is bad");
+        }
+    }
+}
