65ec0884bc410144dca673d03e629f980f393fad
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2022-07-31 02:11:00 +0530

    String compress using HashMap
    https://my.newtonschool.co/playground/code/c8idvljepoek/
    
    String Basics - Post Class - Compress String

--- newton/Main.java
+++ newton/Main.java
@@ -1,63 +1,33 @@
-package com.newton;
+package com.savage;
 
-import java.io.*; // for handling input/output
 import java.util.*;
-import java.math.BigInteger; // contains Collections framework
-class Main {
-    static int modInverse(int a, int m)
-    {
-        int m0 = m;
-        int y = 0, x = 1;
+import java.util.HashMap;
 
-        if (m == 1)
-            return 0;
+public class Main {
 
-        while (a > 1) {
+    public static void main(String[] args) {
 
-            int q = a / m;
-            int t = m;
-            m = a % m;
-            a = t;
-            t = y;
-            y = x - q * y;
-            x = t;
+        String myString = "asfajkfbaflabf";
+
+        char[] myCharArr = myString.toCharArray();
+
+
+        HashMap<Character, Integer> myMap = new HashMap<>();
+        int count = 0;
+        for(int i=0; i<myString.length(); i++){
+            if (myMap.containsKey(myCharArr[i])){
+                count++;
+                myMap.put(myCharArr[i],count);
+
+            } else {
+                count = 1;
+                myMap.put(myCharArr[i],count);
+            }
         }
 
-        if (x < 0)
-            x += m0;
-
-        return x;
-    }
-    public static int binomialSum(int n){
-        return (1<<n) - 1;
-    }
-
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        int m= 1000000007;
-        int n= sc.nextInt();
-        int a= sc.nextInt();
-        int b= sc.nextInt();
-
-        int fact []= new int[n+1];
-        fact[0]=1;
-
-        for(int i=1;i<=n;i++){
-            fact[i]= (i*fact[i-1])%m;
+        for (Map.Entry<Character, Integer> entry : myMap.entrySet()) {
+            System.out.print(entry.getKey()+""+entry.getValue());
         }
-        long n1=fact[n];
-        int d1= modInverse(fact [n-a],m);
-        int d2= modInverse(fact [a],m);
-        int d3= modInverse(fact [n-b],m);
-        int d4= modInverse(fact [b],m);
-
-        long nca= ((n1*d1)%m*d2)%m;
-        long ncb= ((n1*d3)%m*d4)%m;
-
-        long candypair= binomialSum(n)-nca-ncb;
-        System.out.println(candypair);
-        // 3c0 3c1 3c2 3c3 === 2^3
-        //
 
     }
 }
